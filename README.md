ef9b7490bfbf6b99b1688d4c68ed349bdafad642
Nathaniel Tripet <ntripet16@gmail.com>
2022-01-26 20:48:11 -0500

    Added point system

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # rock-paper-scissors
+
+Play the classic rock, paper, scissors, shoot! game.
+
+# How to play
+
+Game is played in console against a computer up to the desired number of points. Game does not yet support typos in prompt. In case of typo, refresh the page and start again.
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-console.log("Game has begun.");
 
+// Randomize computer's choice
 function computerPlay() {
     let oneThruThree = Math.floor((Math.random() * 3) + 1);
     if (oneThruThree === 1) {
@@ -14,29 +14,52 @@ function computerPlay() {
     }
 }
 
-function playRound(userSelection, computerSelection) {
+// Logic to decide winner given user's and computer's choice
+function playRound(userSelection, computerSelection, userPoints, computerPoints) {
     let userChoiceNoCaps = userSelection.toLowerCase();
-
+// Checking for tie, then win, then lose. Returns results in a string and updated points for each player 
     if (userChoiceNoCaps === computerSelection) {
-        return `It's a tie! You both chose ${userChoiceNoCaps}.`;
+        return [`It's a tie! You both chose ${userChoiceNoCaps}.`, userPoints, computerPoints];
     } else if ((userChoiceNoCaps === "rock" && computerSelection === "scissors") ||
         (userChoiceNoCaps === "scissors" && computerSelection === "paper") ||
         (userChoiceNoCaps === "paper" && computerSelection === "rock")) {
-        return `You win! ${userChoiceNoCaps} beats ${computerSelection}.`;
+        return [`You win! ${userChoiceNoCaps} beats ${computerSelection}.`, (userPoints += 1), computerPoints];
     } else if ((userChoiceNoCaps === "rock" && computerSelection === "paper") ||
         (userChoiceNoCaps === "paper" && computerSelection === "scissors") ||
         (userChoiceNoCaps === "scissors" && computerSelection === "rock")) {
-        return `You lose. Your ${userChoiceNoCaps} was beaten by ${computerSelection}.`;
-        } else {
+        return [`You lose. Your ${userChoiceNoCaps} was beaten by ${computerSelection}.`, userPoints, (computerPoints += 1)];
+    } else {
         return "playRound Error";
         }
 }
 
-let computerSelection = computerPlay();
-// let userSelection = prompt("Rock, Paper, or Scissors?");
+function game(userPoints = 0, computerPoints = 0) {
+    computerSelection = computerPlay();
+    userSelection = prompt("Rock, Paper, or Scissors?"); 
+    let gameArray = playRound(userSelection, computerSelection, userPoints, computerPoints);
+    console.log(gameArray[0]);
+    console.log("Your points: " + gameArray[1]);
+    console.log("Enemy's points: " + gameArray[2]);
+//Plays until desired points
+    if ((gameArray[1] !== playUntilPoints) && (gameArray[2] !== playUntilPoints)) {
+    console.log(" ");
+    game (gameArray[1], gameArray[2]);
+    } else {
+    return;
+    }
+} 
+
+let playUntilPoints = Number(prompt("How many points would you like to play to? Enter a number."));
+console.log(`Game has begun. Playing to ${playUntilPoints} points.`);
+console.log(" ");
+
+game();
 
 
 
-console.log(playRound(userSelection, computerSelection));
 
-let userSelection = prompt("Rock, Paper, or Scissors?");
+
+
+
+
+
